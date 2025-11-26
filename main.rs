81ae2bbefb6280a815157d996d5719a53c49c3d6
Christian Lentz <77479171+ChristianLentz@users.noreply.github.com>
2025-11-26 12:56:37 -0500

    uploading oat_rust directory

---
+++ main.rs
@@ -0,0 +1,15 @@
+
+///
+/// To run `main.rs`, navigate to the `oat_rust` directory and run: 
+/// 
+/// 1) `cargo build --release` to build an executible 
+/// 2) `./target/release/oat_rust` to run the executible
+/// 
+/// Note that release will take longer to compile, but will provide an optimized executible. If you are 
+/// looking to run in debug mode, use the standard `cargo build`.
+/// 
+fn main() {
+
+    println!("\n Running main function . . . \n");
+
+}
